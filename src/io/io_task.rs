1e9bd24a8f2d7d59d488d32ddc59504eb057f21c
Tobias Klockau <tobias.klockau@deutschebahn.com>
2024-04-04 21:30:26 +0200

    implement task parsing

--- src/io/io_task.rs
+++ src/io/io_task.rs
@@ -1,9 +1,30 @@
 use serde::Deserialize;
 use std::collections::HashMap;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct IoTask {
     pub topic: Option<String>,
     pub attempts: Vec<String>,
     pub subtasks: HashMap<String, IoTask>
+}
+
+#[cfg(test)]
+pub mod test_defaults {
+    use super::*;
+    impl IoTask {
+        pub fn test_default1() -> IoTask {
+            IoTask {
+                topic: Some("Vectors".to_owned()),
+                attempts: vec!["h".to_owned(), "v".to_owned()],
+                subtasks: HashMap::from([("a)".to_owned(), IoTask::test_default2())])
+            }
+        }
+        pub fn test_default2() -> IoTask {
+            IoTask {
+                topic: Some("Tractors".to_owned()),
+                attempts: vec!["9/11".to_owned()],
+                subtasks: HashMap::new()
+            }
+        }
+    }
 }
--- src/structs/task.rs
+++ src/structs/task.rs
@@ -1,9 +1,65 @@
 use std::collections::HashMap;
 
-use super::attempt::Attempt;
+use super::attempt::{Attempt, UnsupportedAttemptStringError};
+use super::super::io::io_task::IoTask;
 
+#[derive(Debug, PartialEq)]
 pub struct Task {
     pub topic: Option<String>,
     pub attempts: Vec<Attempt>,
     pub subtasks: HashMap<String, Task>
+}
+impl Task {
+    pub fn parse(io_task: &IoTask) -> Result<Task, UnsupportedAttemptStringError> {
+        let mut attempts = Vec::new();
+        for s in &io_task.attempts {
+            attempts.push(Attempt::parse(s)?);
+        }
+
+        let mut subtasks = HashMap::new();
+        for (k, v) in &io_task.subtasks {
+            subtasks.insert(k.to_owned(), Task::parse(v)?);
+        }
+
+        Ok(Task {
+            topic: io_task.topic.clone(),
+            attempts,
+            subtasks
+        })
+    }
+}
+
+#[cfg(test)]
+pub mod test_defaults {
+    use super::*;
+    impl Task {
+        pub fn test_default1() -> Task {
+            Task {
+                topic: Some("Vectors".to_owned()),
+                attempts: vec![Attempt::WithHelp, Attempt::Correct],
+                subtasks: HashMap::from([("a)".to_owned(), Task::test_default2())])
+            }
+        }
+        pub fn test_default2() -> Task {
+            Task {
+                topic: Some("Tractors".to_owned()),
+                attempts: vec![Attempt::PartiallyCorrect(9, 11)],
+                subtasks: HashMap::new()
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(
+            Task::parse(&IoTask::test_default1()).unwrap(),
+            Task::test_default1()
+        )
+    }
+
 }
