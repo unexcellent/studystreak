13b861e3401bcdf21adee61dd811da509ecc0604
Tobias Klockau <tobias.klockau@deutschebahn.com>
2024-04-04 21:40:57 +0200

    implement sheet parsing

--- src/io/io_sheet.rs
+++ src/io/io_sheet.rs
@@ -3,9 +3,23 @@ use serde::Deserialize;
 
 use super::io_task::IoTask;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, PartialEq)]
 pub struct IoSheet {
     pub tasks_path: PathBuf,
     pub solutions_path: Option<PathBuf>,
     pub tasks: HashMap<String, IoTask>,
+}
+
+#[cfg(test)]
+pub mod test_defaults {
+    use super::*;
+    impl IoSheet {
+        pub fn test_default1() -> IoSheet {
+            IoSheet {
+                tasks_path: PathBuf::from("/path/to/tasks.pdf"),
+                solutions_path: Some(PathBuf::from("/path/to/solutions.pdf")),
+                tasks: HashMap::from([("1.".to_owned(), IoTask::test_default1())]),
+            }
+        }
+    }
 }
--- src/structs/sheet.rs
+++ src/structs/sheet.rs
@@ -1,10 +1,54 @@
 use std::{collections::HashMap, path::PathBuf};
 
-use super::task::Task;
+use crate::io::io_sheet::IoSheet;
 
+use super::task::Task;
+use super::attempt::UnsupportedAttemptStringError;
+
+#[derive(Debug, PartialEq)]
 /// An exercise sheet (a single pdf file containing tasks)
 pub struct Sheet {
     pub tasks_path: PathBuf,
     pub solutions_path: Option<PathBuf>,
     pub tasks: HashMap<String, Task>,
+}
+impl Sheet {
+    pub fn parse(io_sheet: IoSheet) -> Result<Sheet, UnsupportedAttemptStringError> {
+        let mut tasks = HashMap::new();
+        for (k, v) in &io_sheet.tasks {
+            tasks.insert(k.to_owned(), Task::parse(v)?);
+        }
+        Ok( Sheet {
+            tasks_path: io_sheet.tasks_path,
+            solutions_path: io_sheet.solutions_path,
+            tasks,
+        } )
+    }
+}
+
+#[cfg(test)]
+pub mod test_defaults {
+    use super::*;
+    impl Sheet {
+        pub fn test_default1() -> Sheet {
+            Sheet {
+                tasks_path: PathBuf::from("/path/to/tasks.pdf"),
+                solutions_path: Some(PathBuf::from("/path/to/solutions.pdf")),
+                tasks: HashMap::from([("1.".to_owned(), Task::test_default1())]),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        assert_eq!(
+            Sheet::parse(IoSheet::test_default1()).unwrap(),
+            Sheet::test_default1()
+        )
+    }
 }
