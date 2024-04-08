8f3de968ae4e12f6e528931a1654b85057515676
unexzellent <edu@klockau.com>
2024-04-08 23:43:39 +0200

    implement simple module_grid pic

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,41 @@
-#![allow(dead_code, unused_variables, unused_mut)] // TODO: remove
+#![allow(dead_code, unused_variables, unused_mut, unused_assignments)] // TODO: remove
+
+use std::{path::PathBuf, rc::Rc};
+use std::collections::HashMap;
 slint::include_modules!();
 
 #[path ="structs/mod.rs"]
 mod structs;
+use slint::{ModelRc, SharedString, VecModel};
+use structs::module::Module;
 
 #[path ="io/mod.rs"]
 mod io;
 
-use std::path::PathBuf;
-
-const IO_PATH: &str = "~/.studystreak.yaml";
 
 fn main() -> Result<(), slint::PlatformError> {
-    let io_path = PathBuf::from(IO_PATH);
+    let ui = AppWindow::new()?;
+
+    let io_path = PathBuf::from(format!("{}/.studystreak.json5", std::env!("HOME")));
     let mut modules = io::init_io::init_io(&io_path);
 
-    let ui = AppWindow::new()?;
+    populate_start_page(&modules, &ui);
+
     ui.run()
+}
+
+fn populate_start_page(modules: &HashMap<String, Module>, ui: &AppWindow) {
+    let mut slint_modules: Rc<VecModel<SlintModule>> = Rc::new(VecModel::default());
+    for (name, module) in modules {
+        slint_modules.push(
+            SlintModule {
+                name: SharedString::from(name),
+                progress: ProgressValues {correct: 1, with_help: 2, incorrect: 3}
+            }
+        );
+    }
+
+    assert_eq!(modules.len(), 11);
+
+    ui.global::<State>().set_modules(ModelRc::from(slint_modules));
 }
